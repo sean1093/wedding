d27f60fe228f8913dd8ec57567545302060cf6a0
Sean Chou <sean1093@gmail.com>
2022-03-25 22:22:58 +0800

    feat: add router on each pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Main from './service/main/Main';
+import Register from './service/register/Register';
+import Search from './service/search/Search'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/register" element={<Register />} />
+                <Route path="/search" element={<Search />} />
+                <Route path="/" element={<Main />}>
+            </Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
