d6ae0e29609d4ad8f8c6982767bafe4971d90321
Sean Chou <sean1093@users.noreply.github.com>
2022-03-31 23:00:01 +0800

    Add Google Sheets and connect to Apps Script (#13)
    * chore: enable Google sheets service
    
    * feat: connect the Apps Script and Google sheets

---
+++ appsScript/config.js
@@ -0,0 +1,3 @@
+const SHEET_ID = '19jxjB1DIp2c7oTRCR_TzCfe99tpy0Eb4vP_yHosQrMQ';
+const SHEET_RANGE = 'reply!A2:B';
+
--- appsScript/main.js
+++ appsScript/main.js
@@ -1,3 +1,10 @@
 function myFunction() {
-  // hello test update
+  // test get data
+  const rows = Sheets.Spreadsheets.Values.get(SHEET_ID, SHEET_RANGE).values;
+
+  for(let i=0; i<rows.length; i++) {
+    for(let j=0; j<rows[i].length; j++) {
+      console.log(rows[i][j])
+    }
+  }
 }
