b1560a0d35beb1eae1b32947cdcf4e4303be1466
Sean Chou <sean1093@gmail.com>
2022-04-25 22:28:07 +0800

    feat: add common header

---
+++ src/service/main/Header.js
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+import { Link } from "react-router-dom";
+
+import Box from '../../components/Box';
+import Flex from '../../components/Flex';
+import Text from '../../components/Text';
+
+import content from '../../assets/content.json';
+
+const StyleLink = styled(Link)`
+    text-decoration: none;
+    color: #E4B392;
+    padding: 16px;
+`;
+
+const Header = () => {
+    return (
+        <Box
+            width="100%"
+            minWidth="330px"
+            height="50px"
+            borderBottom="1px solid #E4B392"
+        >
+            <Flex justify="space-between" height="50px">
+                <Text fontSize="24px" padding="12px">
+                    {content.header.title}
+                </Text>
+                <Flex>
+                    <StyleLink to="/">{content.header.home}</StyleLink>
+                    <StyleLink to="/register">{content.header.register}</StyleLink>
+                </Flex>
+            </Flex>
+        </Box>
+    )
+};
+
+export default Header;
