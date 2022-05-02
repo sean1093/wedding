b5611dfa5f5fd79b79ddccb754a32a45be87855b
Sean Chou <sean1093@gmail.com>
2022-05-02 10:56:35 +0800

    chore: deploy first draft version

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Main from './service/main/Main';
 import Register from './service/register/Register';
 import Search from './service/search/Search'
 
 function App() {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
                 <Route path="/register" element={<Register />} />
                 <Route path="/search" element={<Search />} />
                 <Route path="/" element={<Main />}>
             </Route>
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     );
 }
 
