aec0df7ca43191a717891bde05cbed7d4f9063e5
Sean Chou <sean1093@users.noreply.github.com>
2022-05-07 22:01:23 +0800

    chore: update API version (#44)

--- src/config/apiService.js
+++ src/config/apiService.js
@@ -1,1 +1,1 @@
-export const API_HOST = 'https://script.google.com/macros/s/AKfycbw7ltWskXWeAYdKJbP58SIvu8vUN7IquIa8nIZnD3VgcnZo2bRMGTeKZIIM9KOVarCT/exec';
+export const API_HOST = 'https://script.google.com/macros/s/AKfycbw-RgNmZWFrk9uwJKBIk2iw5ZB8S9mi2MISiLkUNT8XMFxa5z7TuvTI9nhhdmSJsTT6/exec';
