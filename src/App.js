94bba75d9a2b2e2d4d27e53468c4f5294dc1f682
Sean Chou <sean1093@gmail.com>
2022-06-04 11:08:47 +0800

    feat: add images preload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { HashRouter, Routes, Route } from 'react-router-dom';
+
 import Main from './service/main/Main';
 import Register from './service/register/Register';
 import Search from './service/search/Search';
+
+import ImageMain from './assets/images/main.png';
+import ImageFinished from './assets/images/finished.png';
+
 import './App.css';
 
+const preloadImages = [{ src: ImageMain }, { src: ImageFinished }];
+
 function App() {
+    useEffect(() => {
+        // preloading image
+        preloadImages.forEach((image) => {
+            const img = new Image();
+            img.src = image.src;
+        });
+    }, []);
     return (
         <HashRouter>
             <Routes>
