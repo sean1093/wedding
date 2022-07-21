4a12ba7abcb5785780e3bfedbc2fb989a85612d5
Sean Chou <sean1093@gmail.com>
2022-07-21 21:31:07 +0800

    chore: update API version v19

--- src/config/apiService.js
+++ src/config/apiService.js
@@ -1,2 +1,3 @@
+const API_KEY = 'AKfycbytAc1eKCFQ_6TFI43KzgqnmxxFb_x5RXs-kfe9iSmxagh1pIqzNCP4YUugLGWIPzzT';
 export const API_HOST =
-    'https://script.google.com/macros/s/AKfycbz4OndkEsASjB8lViMIhk5BJ6ozCmhoCJgOVtAsMXBU25gpejv7O2DlJy5aRKFA94M/exec';
+    `https://script.google.com/macros/s/${API_KEY}/exec`;
